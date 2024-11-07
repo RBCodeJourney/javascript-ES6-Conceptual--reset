c6dda4ba0b5df52f849ff991516b1310692f1904
RBCodeJourney <refatullahbiswas@gmail.com>
2024-11-08 00:23:13 +0530

    spreadArray.js-file completed

---
+++ spreadArray.js
@@ -0,0 +1,27 @@
+// const number = [2, 4, 1];
+
+
+// const number1 = [...number];
+// // const number1 = [];
+// // for (let num of number) {
+// //     number1.push(num)
+// // }
+
+
+
+// number.push(10);
+// number1.push(20);
+// // number1.push(30);
+
+// console.log('number: ', number);
+// console.log('number1: ', number1);
+
+
+const nums = [3, 5, 2, 6, 7, 8, 9];
+// const a = nums[0];
+// const b = nums[1];
+// const c = nums[2]
+
+const [a, b, c, ...d] = nums;
+
+console.log(a, b, c, d);
