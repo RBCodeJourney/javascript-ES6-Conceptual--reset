4c4a7761df9043fe5621a07bfd264e8135904543
RBCodeJourney <refatullahbiswas@gmail.com>
2024-11-07 22:45:43 +0530

    basics.js-file added

--- basics.js
+++ basics.js
@@ -1,1 +1,40 @@
-console.log('im am Biswas')
+// // primitive datatype
+
+// const x = 5;
+// const y = true;
+// const a = "hello";
+
+
+// let z = x;
+// // z = 10;
+// // console.log('x =', x);
+// // console.log('z =', z);
+
+
+
+
+// // non premitive
+
+// const b = { name: "Biswas" };
+// const nums = [1, 2, 3];
+
+
+// let num1 = nums;
+// // num1.push(4);
+// // console.log('num1:', num1)
+// // console.log('nums:', nums)
+
+
+
+// // const c = b;
+// // c.age = 25;
+// // // console.log('c:', c);
+// // // console.log('b:', b);
+
+// truthy and false value 
+
+// falsy value => false,0, "", null, undefine,NaN
+// truthy value => true, 'Biswas', 1234, 1, [],{}
+
+// const value = false;
+// console.log(!!value);
