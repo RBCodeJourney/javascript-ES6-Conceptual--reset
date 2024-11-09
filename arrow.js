cdd1bce1f4861c01c9686ee24bf622621920b91f
RBCodeJourney <refatullahbiswas@gmail.com>
2024-11-10 01:03:28 +0530

    arrow.js file added and arrayMethods.js file created

---
+++ arrow.js
@@ -0,0 +1,25 @@
+// function add(x, y) {
+//     return x + y;
+// };
+
+
+// const add = (x, y) => {
+//     return x + y;
+// }
+
+// const add = (x, y) => x + y;
+
+// const double = (x) => x * 2;
+
+// const double = x => {
+//     const a = x * 2;
+//     return a;
+// }
+// console.log(double(9));
+
+// const sayHello = () => console.log('say Hello to Refatullah Biswas');
+
+// sayHello();
+
+
+
