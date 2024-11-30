5a9be814defd349eb9d79f5f4b682a38c8c41188
RBCodeJourney <refatullahbiswas@gmail.com>
2024-11-30 16:35:37 +0530

    scope.js file added

---
+++ scope.js
@@ -0,0 +1,39 @@
+// global scope 
+// block scope (let, const)
+// local scope or functional scope 
+
+// // 1 global scope 
+// const y = 5;
+
+// if (true) {
+//     console.log(y)
+// }
+
+// console.log(y);
+
+
+// 2. block scope
+
+// {
+//     var y = 10;
+// }
+
+// console.log(y)
+
+
+// function hello() {
+//     let y = 10;
+// }
+
+// hello()
+// console.log(y)
+
+
+// const y = 5;
+
+// {
+//     const y = 10;
+//     console.log(y)
+// }
+
+// console.log(y)
