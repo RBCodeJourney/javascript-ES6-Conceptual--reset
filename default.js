a768cc755620a7a02cd2746a2ec0360dcb77ab86
RBCodeJourney <refatullahbiswas@gmail.com>
2024-12-01 17:39:33 +0530

    defalult.js file added

---
+++ default.js
@@ -0,0 +1,19 @@
+// const add = (x = 0, y = 0) => {
+//     console.log(x + y)
+// }
+
+// add( 3);
+
+
+
+const person = {
+    name: 'Biswas',
+    age: 26
+}
+
+const showInfo = ({name,age}) => {
+    
+    console.log(name, age)
+}
+
+showInfo(person)
